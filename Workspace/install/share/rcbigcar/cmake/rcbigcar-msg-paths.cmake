03f354b34ed720c7586b5a615879b532aec49665
One <hazel@agx.com>
2021-08-04 22:15:04 +0800

    env added, rcbigcar moved into workspace_v2 since it doesn't have an install file

---
+++ Workspace/install/share/rcbigcar/cmake/rcbigcar-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${rcbigcar_DIR}/.." "" rcbigcar_MSG_INCLUDE_DIRS UNIQUE)
+set(rcbigcar_MSG_DEPENDENCIES std_msgs)
--- Workspace/src/rcbigcar/scripts/setup_can.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Set up CAN0
-ip link set can0 type can bitrate 1000000
-ip link set up can0
